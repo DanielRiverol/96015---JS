2a35a6a7f74735475f7a131423c7a3b0a607e054
Daniel Riverol <dannyriverol@hotmail.com>
2026-02-04 19:46:52 -0300

    Fix HTML doctype and add initial JavaScript file with variable examples

---
+++ clase_1/js/main.js
@@ -0,0 +1,41 @@
+// Variables
+// var nombre;//declarando una variable ($ _ )
+// nombre = "Juan"; //asigando un valor
+
+// Inicializacion
+let nombre = "Pedro"; //Cadena de caracteres
+let nombre1;
+// console.log(nombre1);
+
+nombre1 = "Pepe";
+// console.log(nombre1);
+// reasigna el valor a una variable
+nombre1 = "Manuel";
+
+let temperatura = 35.4;
+// console.log(temperatura);
+
+temperatura = 27; //number
+// console.log(temperatura);
+
+temperatura = "16.8"; //string
+// console.log(temperatura);
+temperatura = 25;
+console.log(temperatura);
+
+// constantes se inicializan
+const GRAVEDAD = 9.8;
+console.log(GRAVEDAD);
+
+let isActive = false;
+console.log(isActive);
+isActive = true;
+console.log(isActive);
+
+let nombreCompleto = "Juanito Arcoiris";
+console.log(nombreCompleto);
+
+// metodos de salida alert/console.log
+// console.log("Pepe");
+// console.log(nombre1);
+// alert(nombreCompleto)
