a6697b9ebf1a3cd6d30a179996cfe3267c83451e
Daniel Riverol <dannyriverol@hotmail.com>
2026-02-11 20:59:04 -0300

    Implement ATM functionality with PIN validation and transaction options

--- Ejercicio integrador/js/main.js
+++ Ejercicio integrador/js/main.js
@@ -13,4 +13,37 @@
 4- Al finalizar mostrar mensaje
 */
 
+let pinGuardado = "5701";
+let saldo = 200000;
+let ingresar = false;
+let ingreso;
+// inicio
+for (let i = 2; i >= 0; i--) {
+  ingreso = prompt("Ingresa tu clave");
+  if (pinGuardado == ingreso) {
+    alert("Bienvenido ya podes operar");
+    ingresar = true;
+    break;
+  } else {
+    alert("Error");
+  }
+}
 
+if (ingresar) {
+  let opcion = prompt(
+    "Elegí una opción: \n1- Saldo. \n2 - Retiro de dinero. \n3 - Depósito. \nPresioná X para finalizar.",
+  );
+  while (opcion != "x") {
+    // switch o un if
+    if (opcion == "1") {
+      alert("Tu saldo es: $" + saldo);
+    }
+    // completar el resto de las opciones
+    // condicion de salida
+    opcion = prompt(
+      "Elegí una opción: \n1- Saldo. \n2 - Retiro de dinero. \n3 - Depósito. \nPresioná X para finalizar.",
+    );
+  }
+} else {
+  alert("Retuvimos tu tarjeta. Anda reclamar a montoto");
+}
