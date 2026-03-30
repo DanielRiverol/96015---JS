660f0fe1a35e947a2fd9fcf2d50e85b342b2caf4
Daniel Riverol <dannyriverol@hotmail.com>
2026-03-30 19:45:25 -0300

    actualizacion

--- clase_12/js/main.js
+++ clase_12/js/main.js
@@ -1,7 +1,7 @@
 const card = document.querySelector(".card");
 const btn = document.querySelector("#btn"),
   contenedor = document.querySelector(".contenedor");
-
+const form = document.querySelector("#form-login");
 const servicios = [
   { id: 1, nombre: "limpieza de cutis", precio: 1200, img: "limpieza.jpg" },
   { id: 2, nombre: "masajes", precio: 1800, img: "masajes.jpg" },
@@ -23,8 +23,6 @@ const servicios = [
     img: "micropigmentacion.jpeg",
   },
 ];
-
-
 
 const renderServicios = (arr) => {
   contenedor.innerHTML = "";
@@ -50,3 +48,79 @@ const renderServicios = (arr) => {
   }
 };
 
+// renderServicios(servicios);
+
+// promesas
+
+// console.log('Hola');
+// console.log(new Promise((resolve,reject)=>{
+//   // cuerpo de la promesa
+// }))
+// console.log('Chau');
+
+const eventoFuturo = (response) => {
+  return new Promise((resolve, reject) => {
+    // cuerpo de la promesa
+    setTimeout(() => {
+      if (response === true) {
+        resolve("Promesa resuelta");
+      } else {
+        reject("Promesa rechazada");
+      }
+    }, 2000);
+  });
+};
+
+// eventoFuturo(true)
+//   .then((res) => console.log(res))
+//   .catch((error) => console.error(error))
+//   .finally(() => {
+//     console.log("Fin");
+//   });
+
+const devolverCD = (respuesta) => {
+  return new Promise((resolve, reject) => {
+    // cuerpo de la promesa
+    setTimeout(() => {
+      if (respuesta === "si") {
+        resolve("Si ya te lo devuelvo");
+      } else {
+        reject("NO, la verdad que no lo tengo");
+      }
+    }, 3000);
+  });
+};
+
+// console.log("Te acordas el cd que te presté?");
+
+// devolverCD("si")
+//   .then((res) => console.log(res))
+//   .catch((error) => console.log(error))
+//   .finally(() => {
+//     console.log("Dale, gracias");
+//   });
+
+//   console.log("Sigo haciendo cosas");
+
+const pedirServicios = (arr) => {
+  return new Promise((resolve, reject) => {
+    // cuerpo de la promesa
+    setTimeout(() => {
+      if (arr) {
+        resolve(arr);
+      } else {
+        reject("Error al cargar los datos");
+      }
+    }, 3000);
+  });
+};
+
+pedirServicios(servicios)
+  .then((response) => {
+    // console.log(response);
+    // renderServicios(response)
+  })
+  .catch((error) => {
+    console.log(error);
+    contenedor.innerHTML=`<h2>${error}</h2>` 
+  });
